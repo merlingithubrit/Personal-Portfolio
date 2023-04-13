747e5eb44ea31bfe989e2411e47008efab98aa09
P.Merlin Roselin Mary <128605226+merlingithubrit@users.noreply.github.com>
2023-04-13 16:24:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//Header Toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+//Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings:['Front-End Developer','Back-End Developer','Mobile Apps Process Supporter'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+})
+//Active Link State On Scroll
+
+//Get All Links
+let navLinks = document.querySelectorAll('nav ul li a')
+//Get All Sections
+let section = document.querySelectorAll('section')
+console.log(section)
+
+window.addEventListener('scroll',function(){
+    const scrollPos = this.window.scrollY
+    section.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos <(section.offsetTop + section.offsettleHeight)){
+            navLinks.forEach(link =>{
+                link.classList.remove('active');
+                if(section.getAttribute('id')===link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
